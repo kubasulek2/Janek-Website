467c6829f168114760a2c0e252e18ef6946374ea
Kuba Sukowski <kubasulek2@gmail.com>
2019-04-03 17:56:10 +0200

    gallery functions done

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,108 @@
 $(() => {
 
-  // mobile browser check
-  window.mobilecheck = function() {
-    var check = false;
-    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
-    return check;
+  const handleLeftGallery = (index, images)=>{
+    let counter = index;
+
+    $(".nav-left > .next").on("click", function () {
+
+      if (counter === index ){
+        $(this).prev().show()
+      }
+
+      $(images[counter]).hide();
+      counter += 2;
+      $(images[counter]).show();
+
+      if( !(images.length % 2) && counter === images.length -2 ){
+        $(this).hide();
+      } else if( images.length % 2 && counter === images.length -1){
+        $(this).hide()
+      }
+
+    });
+
+    $(".nav-left > .prev").on("click", function () {
+
+      console.log(counter);
+
+      if( !(images.length % 2) && counter === images.length -2 ){
+        $(this).next().show();
+
+      } else if( images.length % 2 && counter === images.length -1){
+        $(this).next().show()
+      }
+
+      $(images[counter]).hide();
+      counter -= 2;
+      $(images[counter]).show();
+
+      if (counter === index ){
+        $(this).hide()
+      }
+
+    })
   };
 
-  console.log(window.mobilecheck());
+  const handleRightGallery = (index, images)=>{
+    let counter = index;
+
+    $(".nav-right > .next").on("click", function () {
+
+      if (counter === index ){
+        $(this).prev().show()
+      }
+
+      $(images[counter]).hide();
+      counter += 2;
+      $(images[counter]).show();
+
+      if( !(images.length % 2) && counter === images.length -1 ){
+        $(this).hide();
+      } else if( images.length % 2 && counter === images.length -2){
+        $(this).hide()
+      }
+
+    });
+
+    $(".nav-right > .prev").on("click", function () {
+
+
+      if( !(images.length % 2) && counter === images.length -1 ){
+        $(this).next().show();
+
+      } else if( images.length % 2 && counter === images.length -2){
+        $(this).next().show()
+      }
+
+      $(images[counter]).hide();
+      counter -= 2;
+      $(images[counter]).show();
+
+      if (counter === index ){
+        $(this).hide()
+      }
+
+    })
+  };
+
+  const showImagesCommercial = () => {
+    const images = $('.image');
+
+    let leftIndex = 0;
+    let rightIndex = 1;
+
+    $(images[0]).show();
+    $(images[1]).show();
+   handleLeftGallery(leftIndex, images);
+   handleRightGallery(rightIndex, images);
+
+
+ };
+ showImagesCommercial();
+
+
+
+//commercial left gallery
+
 });
+
