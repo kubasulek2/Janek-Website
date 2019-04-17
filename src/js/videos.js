d9d247a396e2a56310a649e44d7c92a61d7d0a08
Kuba Sukowski <kubasulek2@gmail.com>
2019-04-17 16:52:45 +0200

    videos height calculated from data-ratio

--- src/js/videos.js
+++ src/js/videos.js
@@ -6,18 +6,27 @@ $(()=>{
   $videos.on('click', function (e) {
     e.stopPropagation();
 
-
     const vidBackground = $('<div class="vid-background"></div>');
+
+
     const vidContainer = $('<div class="vid-container"></div>');
     const iframe = $('<iframe></iframe>',{ frameborder:"0", allowfullscreen:'', webkitallowfullscreen:'', mozallowfullscreen:''})
       .attr('src', $(this).data("src"));
-
     vidContainer.append(iframe);
+
     vidBackground.append(vidContainer);
     $main.append(vidBackground);
     $(this).attr("class").includes("odd") ? vidContainer.addClass("odd"):vidContainer.addClass("even");
+    $(this).attr("class").includes("small") || $(this).attr("class").includes("small") ? vidContainer.addClass("small"): null;
+
+    const videoRatio = $(this).attr("data-ratio");
+    const videoWidth = vidContainer.css("width");
+    const videoHeight = parseFloat(videoWidth) * parseFloat(videoRatio)/100;
+    vidContainer.css("height", videoHeight);
+    iframe.css("height", videoHeight);
+
     $body.one('click', function () {
       vidBackground.remove();
     })
-   })
+  })
 });
