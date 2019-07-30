8b79a96c2be22da422bc51de37b39bdade513c45
Kuba Sukowski <kubasulek2@gmail.com>
2019-07-30 19:00:06 +0200

    site done:DDD

--- src/js/app.js
+++ src/js/app.js
@@ -304,7 +304,7 @@ $(() => {
 					if (el === topVideo) {
 						//moze lepiej
 						// video.paused && video.readyState === 4 ? video.play() : null;
-						video.paused && video.readyState === 4 ? video.play() : null;
+						video.paused ? video.play() : null;
 					} else {
 						!video.paused ? video.pause(): null;
 					}
